ddaf31614a9d7b61f632cd7d63a1eeb58f39e052
name <minaary@mailfence.com>
2022-06-10 17:47:23 +0200

    remove navbar object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('something something');
+'use strict';
+import { navbarDiv } from "./layout/navbar";
+//import { Navbar } from "./layout/navbar";
+
+const contentWrap = document.querySelector('#js-content');
+contentWrap.appendChild(navbarDiv);
+
+// okaaaay. but. I can't just append nav links to the content wrapper, I need some kind of containter. Also, I think there should be a better way to append these links than calling the function three times. Ummmm. Lemme think. Oh right rest syntax
