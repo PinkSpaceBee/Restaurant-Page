353e65dfac2a2d2a1c7f9f9cbea290f255c71fbc
name <minaary@mailfence.com>
2022-06-20 11:15:43 +0200

    tidy up a bit

--- src/index.js
+++ src/index.js
@@ -4,43 +4,28 @@ import { mainSection } from "./main-page";
 import { menu } from "./menu";
 import { contacts } from "./contacts";
 import { footerText } from "./layout/footer";
+import { navbar } from "./layout/navbar";
 
 const contentWrap = document.querySelector('#js-content');
 contentWrap.appendChild(navbarDiv);
 contentWrap.appendChild(mainSection);
+
+// footer
 document.querySelector('body').appendChild(footerText);
 
-function addDiv(wrapper, className, ...elems) {
-    for (const elem of elems) {
-        let temp = elem.classList.contains(className) ? elem : '';
-        console.log(temp);
-
-        if (temp.tagName === 'SECTION') {
-            wrapper.appendChild(temp);
-        };
-    }
-}
-
-function removeAllChildNodes(parent) {
+function removeChildNodes(parent) {
     while (parent.childNodes.length > 1) {
         parent.removeChild(parent.lastChild);
     }
 }
 
-function highlightNavlink() {
-}
-
+// well, a list of navigation links
 const linksList = Array.from(document.querySelectorAll('.navlink'));
 
+// switch between the sections 
 linksList.forEach(link => link.addEventListener('click', () => {
-    highlightNavlink();
-    removeAllChildNodes(contentWrap);
-    addDiv(contentWrap, link.classList[0], contacts, menu, mainSection);
+    removeChildNodes(contentWrap);
+    navbar.highlightNavlink();
+    navbar.addDiv(contentWrap, link.classList[0], contacts, menu, mainSection);
 }))
-
-// okay great this shit works. I can switch between the sections on click. Buuuut the question is how do I organize this code? Do I um create a class here or something? 
-// oh wait shouldn't the main page be the default one? 
-
-
-
 
