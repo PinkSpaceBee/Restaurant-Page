c9d8dc9b2c17bd45b318d5f9e2225ecd090b9138
name <minaary@mailfence.com>
2022-06-21 19:24:50 +0200

    add main page content

---
+++ src/main-page.js
@@ -0,0 +1,13 @@
+const home = document.createElement('section');
+home.classList.add('home-nav');
+
+home.appendChild(document.createElement('p')).textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+
+home.appendChild(document.createElement('img')).src = './img/cake-main-page.jpg';
+
+const menuBtn = home.appendChild(document.createElement('button')).textContent = 'Open menu';
+
+home.appendChild
+export {
+    home
+}
